123b7b79f0e5e2629e0a9710e8ee65d4cbfe3ccb
Shreyan Parajuli <mail@shreyanparajuli.com.np>
2024-06-26 22:14:57 +0545

    added dropdown menu

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+document 
+
+function function2() {
+    var dropdowncnt = document.getElementById('dropdown');
+    dropdowncnt.classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
